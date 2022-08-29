77d5ad2399f5ba9e0343c30ecbe2ba531d4fd220
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-30 09:49:04 +1000

    Add files via upload

--- manageInfra.sh
+++ manageInfra.sh
@@ -454,10 +454,10 @@ tagname=$3
 
 if [ "$action" = "create" ]; then  
 	manageAll "network" "$targetRegion"	"false"	"true" "false"
-	#manageAll "application" "$targetRegion" "false" "true" "false"
-	#manageAll "database" "$targetRegion" "false" "true" "false"
-	#manageAll "cache" "$targetRegion" "false" "true" "false"
-	#manageAll "message" "$targetRegion" "false" "true" "false"
+	manageAll "application" "$targetRegion" "false" "true" "false"
+	manageAll "database" "$targetRegion" "false" "true" "false"
+	manageAll "cache" "$targetRegion" "false" "true" "false"
+	manageAll "message" "$targetRegion" "false" "true" "false"
 elif [ "$action" = "destroy" ]; then
 	#manageAll "message" "$targetRegion" "true" "false" "false"
 	#manageAll "cache" "$targetRegion" "true" "false" "false"
